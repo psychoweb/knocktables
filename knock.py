ddea4d6ce23ec348f3be45fedaeb0caf66e10d48
Paolo Serra <paolo.serra@buongiorno.com>
2012-01-22 23:26:10 +0100

    Added 'idseq' mode. Thanks to the u32 iptables module I am now able to inspect arbitrary bytes in the IP packet keeping the Ping packet format 4 bytes at once (but they're enough for now)

--- knock.py
+++ knock.py
@@ -10,7 +10,7 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('--verbose','-v',action='store_true')
 parser.add_argument('target',metavar='TARGET')
-parser.add_argument('args',metavar='PORT[:PROTOCOL]',nargs='+')
+parser.add_argument('sequence',metavar='TOKEN[:MODE]',nargs='+')
 arguments = parser.parse_args()
 
 target = arguments.target
@@ -21,36 +21,49 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-for i in range (0,len(arguments.args)):
-    arguments.args_splitted = arguments.args[i].split(':')
-    if len(arguments.args_splitted) > 2:
-      print("Extra colon found (%s)"%(arguments.args[i]))
+for i in range (0,len(arguments.sequence)):
+    sequence_splitted = arguments.sequence[i].split(':')
+    if len(sequence_splitted) > 2:
+      print("Extra colon found (%s)"%(arguments.sequence[i]))
       sys.exit(1)
-    elif len(arguments.args_splitted) == 2:
-      sequence.append({ 'port': arguments.args_splitted[0], 'protocol': arguments.args_splitted[1]})
+    elif len(sequence_splitted) == 2:
+      sequence.append({ 'token': sequence_splitted[0], 'mode': sequence_splitted[1]})
+#auto-guess mode
     else:
-      if '/' in arguments.args[i]:
-        sequence.append({ 'port': arguments.args_splitted[0], 'protocol': 'icmp'})
+      if '/' in arguments.sequence[i]:
+        sequence.append({ 'token': sequence_splitted[0], 'mode': 'icmp'})
+      elif arguments.sequence[i].startswith('0x'):
+        sequence.append({ 'token': sequence_splitted[0], 'mode': 'idseq'})
       else:
-        sequence.append({ 'port': arguments.args_splitted[0], 'protocol': 'tcp'})
+        sequence.append({ 'token': sequence_splitted[0], 'mode': 'tcp'})
 
 
 for s in sequence:
   if verbose:
-    print("knocking on %s:%s (%s)"%(target,s['port'],s['protocol']))
+    print("knocking on %s:%s (%s)"%(target,s['token'],s['mode']))
   try:
-    if s['protocol'] == 'tcp':
-      tcp.connect((target,int(s['port'])))
-    elif s['protocol'] == 'udp':
-      udp.sendto('\n',(target,int(s['port'])))
+    if s['mode'] == 'tcp':
+      tcp.connect((target,int(s['token'])))
+    elif s['mode'] == 'udp':
+      udp.sendto('\n',(target,int(s['token'])))
   except socket.error:
     pass
-  if s['protocol'] == 'icmp':
+  if s['mode'] == 'icmp':
     icmp = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.getprotobyname('icmp'))
-    icmp_type, icmp_code = s['port'].split('/')
+    icmp_type, icmp_code = s['token'].split('/')
     icmp_checksum =  (~((int(icmp_type) * 256 ) + int(icmp_code)) ) & 0xffff
-    icmp.connect((target,22))
+    icmp.connect((target,42))
     icmp.send(struct.pack('!BBHI',int(icmp_type),int(icmp_code),icmp_checksum,0))
     icmp.close()
     #introduce a small delay not to overlay ICMP packets
     time.sleep(0.05)
+  elif s['mode'] == 'idseq':
+    icmp = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.getprotobyname('icmp'))
+    icmp_idseq = s['token']
+    icmp_checksum =  (~((8 * 256 ) + 0 + (int(icmp_idseq,16)) ) ) & 0xffff
+    icmp.connect((target,42))
+    icmp.send(struct.pack('!BBHI',8,0,icmp_checksum,int(icmp_idseq,16)))
+    icmp.close()
+    #introduce a small delay not to overlay ICMP packets
+    time.sleep(0.05)
+   
--- knocktables.py
+++ knocktables.py
@@ -3,72 +3,98 @@
 import sys
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description='It creates a safe port-knocking rules sequence to be used with iptables.\nICMP is supported (specify it with \"type/code:icmp\" (i.e. \"8/5:icmp\")')
-parser.add_argument("args",metavar="SEQUENCE_PORT[:SEQUENCE_PROTOCOL]",nargs='+')
-parser.add_argument("args",metavar="TARGET_PORT[:TARGET_PROTOCOL]", action='append')
+
+parser = ArgumentParser(description='Creates a safe port-knocking rules sequence to be used with iptables.',
+                        epilog="SEQUENCE_TOKEN is an integer port number for MODE 'tcp' or 'udp', or a 4 bytes hex value\
+                          for MODE 'idseq' (that forms the 'Identifier' and 'Sequence Number' fields of the ICMP ping packet)")
+parser.add_argument("sequence",metavar="SEQUENCE_TOKEN[:MODE]",nargs='+')
+parser.add_argument("sequence",metavar="TARGET_PORT[:TARGET_PROTOCOL]", action='append')
 arguments = parser.parse_args()
 
-
+#removing 'icmp' mode as idseq is more reliable
+allowed_modes = ('tcp','udp','idseq')
 sequence = []
-for i in range (0,len(arguments.args)):
-    args_splitted = arguments.args[i].split(':')
-    if len(args_splitted) > 3:
-      print("Extra colon found (%s)"%(arguments.args[i]))
+
+#set default values
+for i in range (0,len(arguments.sequence)):
+    sequence_splitted = arguments.sequence[i].split(':')
+    if len(sequence_splitted) > 3:
+      print("Extra colon found (%s)"%(arguments.sequence[i]))
       sys.exit(1)
-    elif len(args_splitted) == 3:
-      sequence.append({ 'port': args_splitted[0], 'protocol': args_splitted[1], 'time': args_splitted[2]})
-    elif len(args_splitted) == 2:
-      sequence.append({ 'port': args_splitted[0], 'protocol': args_splitted[1], 'time': '5'})
+    elif len(sequence_splitted) == 3:
+      sequence.append({ 'token': sequence_splitted[0], 'mode': sequence_splitted[1], 'time': sequence_splitted[2]})
+    elif len(sequence_splitted) == 2:
+      sequence.append({ 'token': sequence_splitted[0], 'mode': sequence_splitted[1], 'time': '5'})
     else:
-      if '/' in arguments.args[i]:
-        sequence.append({ 'port': args_splitted[0], 'protocol': 'icmp', 'time': '5'})
+#auto-guess modes
+      if '/' in arguments.sequence[i]:
+        sequence.append({ 'token': sequence_splitted[0], 'mode': 'icmp', 'time': '5'})
+      elif arguments.sequence[i].lower().startswith('0x'):
+        sequence.append({ 'token': sequence_splitted[0], 'mode': 'idseq', 'time': '5'})
       else:
-        sequence.append({ 'port': args_splitted[0], 'protocol': 'tcp', 'time': '5'})
+        sequence.append({ 'token': sequence_splitted[0], 'mode': 'tcp', 'time': '5'})
 
 #parameters sanity check
 for s in sequence:
-  if s['protocol'] not in ('tcp','udp','icmp'):
-    print('Invalid protocol specified (%s). Must be one of "tcp","udp" or "icmp"'%(s['protocol']))
+  if s['mode'] not in allowed_modes:
+    print('Invalid mode specified (%s). Must be one of %s'%(s['mode'],allowed_modes))
     sys.exit(1)
-  if s['protocol'] in ('tcp','udp') and not (s['port'].isdigit() and int(s['port']) in range(0,65536)):
-    print('Invalid port specified (%s)'%(s['port']))
+  if s['mode'] in ('tcp','udp') and not (s['token'].isdigit() and int(s['token']) in range(0,65536)):
+    print('Invalid token specified (%s)'%(s['token']))
     sys.exit(1)
-  if s['protocol'] == 'icmp':
-    icmp_split = s['port'].split('/')
+  if s['mode'] == 'icmp':
+    icmp_split = s['token'].split('/')
     if len(icmp_split) != 2 or not all(p.isdigit() and int(p) in range(0,256) for p in icmp_split):
-      print('Invalid icmp specification (%s). Must be in form of "type/code"\n(with "type" and "code" between 0 and 255)'%(s['port']))
+      print('Invalid icmp specification (%s). Must be in form of "type/code"\n(with "type" and "code" between 0 and 255)'%(s['token']))
+      sys.exit(1)
+  if s['mode'] == 'idseq':
+    try:
+      idseq = int(s['token'],16)
+      if idseq > 0xFFFFFFFF :
+        raise ValueError()
+    except ValueError:
+      print('Invalid idseq specified (%s). Must be a valid 4-bytes hex value (ex: 0xF9E80C)'%(s['token']))
       sys.exit(1)
   if not s['time'].isdigit():
     print("Invalid time specified (%s)"%(s['time']))
     sys.exit(1)
-if sequence[-1]['protocol'] not in ('tcp','udp'):
-  print('Target (last sequence) protocol must be one of "tcp","udp" (it can\'t be "icmp")')
+if sequence[-1]['mode'] not in ('tcp','udp'):
+  print('Target (last sequence) mode must be one of "tcp","udp" (it can\'t be "icmp")')
   sys.exit(1)  
 
 target = sequence[-1]
 first_knock = sequence[0]
 
+#rules preamble
 print("*filter\n"
       ":INPUT ACCEPT [0:0]\n"
       ":FORWARD ACCEPT [0:0]\n"
       ":OUTPUT ACCEPT [0:0]""")
 
 
-#target port goes at the beginning so that it won't interfere with previous similar sequences
+#target token goes at the beginning so that it won't interfere with previous similar sequences
 print("-A INPUT -p %s -m %s --dport %s -m state --state NEW"
       " -m recent --rcheck --name knock_%d --seconds %s --rsource"
       " -m recent --remove --name knock_%d --rsource -j ACCEPT"%
-      (target['protocol'],target['protocol'],target['port'],len(sequence)-2,target['time'],len(sequence)-2))
+      (target['mode'],target['mode'],target['token'],len(sequence)-2,target['time'],len(sequence)-2))
 
-
+#insert rules in reverse order
 for i in range(len(sequence)-2,0,-1):
-  if sequence[i]['protocol'] == 'icmp':
+  if sequence[i]['mode'] == 'icmp':
     print("-A INPUT -p icmp -m icmp --icmp-type %s"
           " -m recent --update --name knock_%d --seconds %s --rsource"
           " -m recent --remove --name knock_%d --rsource"
           " -m recent --set --name knock_%d  --rsource -j ACCEPT"%
-          (sequence[i]['port'],i-1,sequence[i]['time'],i-1,i))
-  elif sequence[i]['protocol'] in ('tcp','udp'):
+          (sequence[i]['token'],i-1,sequence[i]['time'],i-1,i))
+  elif sequence[i]['mode'] == 'idseq':
+    print("-A INPUT -p icmp -m icmp --icmp-type 8/0"
+          " -m u32 --u32 0>>22&0x3C@4&0xFFFFFFFF=%s"
+          " -m recent --update --name knock_%d --seconds %s --rsource"
+          " -m recent --remove --name knock_%d --rsource"
+          " -m recent --set --name knock_%d  --rsource -j ACCEPT"%
+          (sequence[i]['token'],i-1,sequence[i]['time'],i-1,i))
+
+  elif sequence[i]['mode'] in ('tcp','udp'):
     if sequence[i] == target:
       action = "REJECT --reject-with icmp-port-unreachable"
     else:
@@ -77,26 +103,31 @@ for i in range(len(sequence)-2,0,-1):
           " -m recent --update --name knock_%d --seconds %s --rsource"
           " -m recent --remove --name knock_%d --rsource"
           " -m recent --set --name knock_%d  --rsource -j %s"%
-          (sequence[i]['protocol'],sequence[i]['protocol'],sequence[i]['port'],i-1,sequence[i]['time'],i-1,i,action))
+          (sequence[i]['mode'],sequence[i]['mode'],sequence[i]['token'],i-1,sequence[i]['time'],i-1,i,action))
 
 
-if first_knock['protocol'] == 'icmp':
+#first knock goes at the end so that it won't reset similar sequences
+if first_knock['mode'] == 'icmp':
   print("-A INPUT -p icmp -m icmp --icmp-type %s"
         " -m recent --set --name knock_%d --rsource -j ACCEPT"%
-        (first_knock['port'],0))
-elif first_knock['protocol'] in ('tcp','udp'):
+        (first_knock['token'],0))
+elif first_knock['mode'] == 'idseq':
+  print("-A INPUT -p icmp -m icmp --icmp-type 8/0"
+        " -m u32 --u32 0>>22&0x3C@4&0xFFFFFFFF=%s"
+        " -m recent --set --name knock_%d --rsource -j ACCEPT"%
+        (first_knock['token'],0))
+elif first_knock['mode'] in ('tcp','udp'):
   if first_knock == target:
     action = "REJECT --reject-with icmp-port-unreachable"
   else:
     action = "ACCEPT"
-  #first knock goes at the end so that it won't reset similar sequences
   print("-A INPUT -p %s -m %s --dport %s -m state --state NEW"
         " -m recent --set --name knock_%d --rsource -j %s"%(
-        first_knock['protocol'],first_knock['protocol'],first_knock['port'],0,action))
+        first_knock['mode'],first_knock['mode'],first_knock['token'],0,action))
 
-
+#deny target otherwise
 print("-A INPUT -p %s -m %s --dport %s -m state --state NEW"
       " -j REJECT --reject-with icmp-port-unreachable"%(
-      target['protocol'],target['protocol'],target['port']))
+      target['mode'],target['mode'],target['token']))
 print("COMMIT")
 
